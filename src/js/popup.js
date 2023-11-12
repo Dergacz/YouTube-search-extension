2231fb735ae4fb8b4ae2d205ff37d918d6e4b422
Dergacz <sats96dergach@gmail.com>
2023-11-12 18:36:10 +0100

    add close button/ add styles

---
+++ src/js/popup.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchButton = document.getElementById('searchButton');
+    const closeButton = document.getElementById('closeButton');
+    const searchQuery = document.getElementById('searchQuery');
+
+    const onSearchHandler = () => {
+        const query = searchQuery.value;
+        if (query) {
+            const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
+            chrome.tabs.create({ url });
+        }
+    };
+
+    const onClosePopup = () => {
+        window.close();
+    }
+
+    searchQuery.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+            onSearchHandler();
+        }
+    });
+
+    searchButton.addEventListener('click', onSearchHandler);
+    closeButton.addEventListener('click', onClosePopup);
+});
